c1f1cf590a6107b382941a15a12a1d68a2d6f126
Gohgo <gsyed13@hotmail.com>
2023-02-18 00:50:13 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PomodoroClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter how many minutes you would like to study: ");
+            int studyLength = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter how many minutes you would like to relax: ");
+            int relaxLength = int.Parse(Console.ReadLine());
+
+            Timer studyTimer = new Timer(studyLength);
+            Timer relaxTimer = new Timer(relaxLength);
+
+            while (true)
+            {
+                studyTimer.Start();
+                relaxTimer.Start();
+            }
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace PomodoroClock
+{
+    public class Timer
+    {
+        // Fields
+        private int minutes;
+
+        // Constructors
+        public Timer()
+        {
+            minutes = 25;
+        }
+
+        public Timer(int timerLength)
+        {
+            minutes = timerLength;
+        }
+
+        // Function
+        public void Start()
+        {
+            int seconds = minutes * 60;
+
+            while (seconds >= 0)
+            {
+                Console.Write("\r{0}:{1}", seconds / 60, seconds % 60);
+                seconds--;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
